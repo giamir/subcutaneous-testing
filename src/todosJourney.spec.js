206bf0432e33ef9ffeab7787eb73e2019cea8a1a
Giamir Buoncristiani <giamir@thoughtworks.com>
2020-01-14 17:43:33 +0100

    add hints in testing file

--- src/todosJourney.spec.js
+++ src/todosJourney.spec.js
@@ -18,10 +18,31 @@ describe('todos journey', () => {
     });
 
     it('basic usage flow', () => {
+        // user type a todo
+        setTodoInputValue(rootWrapper, 'my first todo');
 
+        // user click the add todo button
+        // user see the added todo in the list
+        // user click on todo to mark it as completed
+        // user see the todo marked as completed
     });
 
     it('visibility filters', () => {
-
+        // user type a todo (todo one)
+        // user click the add todo button
+        // user type another todo (todo two)
+        // user click the add todo button
+        // user click on (todo one) to mark it as completed
+        // user click on active filter
+        // user only see (todo two)
+        // user click on completed filter
+        // user only see (todo one)
+        // user click on all filter
+        // user see all todos
     });
 });
+
+function setTodoInputValue(wrapper, value) {
+    const input = wrapper.find('input');
+    input.instance().value = value;
+}
